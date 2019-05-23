3758d66b07f02b516ca5495e8337ac3e153f8a96
Sofia Huang <HuangSF20@pwcs-edu.org>
2019-05-23 11:32:40 -0400

    fixed

--- src/gitPractice.java
+++ src/gitPractice.java
@@ -1,14 +1,17 @@
 import java.util.*;
+
 public class gitPractice {
     public static void main(String args[]) {
         Scanner in = new Scanner( System.in );
-        System.out.print("What is your name?");
-        String name = in.next();
+        System.out.println("What is your name?");
+        String name = in.nextLine();
         StringBuilder amen = new StringBuilder();
-        for (char c: name.toCharArray()) {
-            amen.append(c);
-            amen.reverse();
+        char[] h = name.toCharArray();
+        for (int i = 0; i < h.length; i++) {
+            amen.append(h[i]); 
         }
+        amen.reverse();
         System.out.print(amen);
     }
 }
+
